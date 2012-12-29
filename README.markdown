74a4eb22f30f26d3885f2693ea56f9e5e24555a3
Andrew Jones <andrewjones86@googlemail.com>
2012-12-29 17:45:11 +0000

    initial commit

---
+++ README.markdown
@@ -0,0 +1,6 @@
+WebService::IMDBAPI
+===================
+
+This is a Perl5 interface to http://imdbapi.org/.
+
+For more information, see https://metacpan.org/module/WebService::IMDBAPI
---
+++ lib/WebService/IMDBAPI.pm
@@ -0,0 +1,20 @@
+use strict;
+use warnings;
+
+package WebService::IMDBAPI;
+
+# ABSTRACT: Interface to http://imdbapi.org/
+
+1;
+
+__END__
+
+=head1 SYNOPSIS
+
+  # TODO
+
+=head1 DESCRIPTION
+
+TODO
+
+=cut
