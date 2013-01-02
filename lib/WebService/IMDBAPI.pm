097ac71e0b21eae7ce8324376fdf0ff5ac27c6c2
Andrew Jones <andrew@arjones.co.uk>
2013-01-02 08:15:01 +0000

    docs

--- lib/WebService/IMDBAPI.pm
+++ lib/WebService/IMDBAPI.pm
@@ -10,8 +10,6 @@ use WebService::IMDBAPI::Result;
 use LWP::UserAgent;
 use JSON;
 
-use Data::Dumper;
-
 # default options
 use constant DEFAULT_USER_AGENT => 'Mozilla/5.0';
 use constant DEFAULT_LANG       => 'en-US';
@@ -86,10 +84,57 @@ __END__
 
 =head1 SYNOPSIS
 
-  # TODO
+  my $imdb = WebService::IMDBAPI->new();
+  
+  # an array of up to 1 result
+  my $results = $imdbapi->search_by_title('In Brugges', { limit => 1 });
+  
+  # an WebService::IMDBAPI::Result object
+  my $result = $results->[0];
+  
+  say $result->title;
+  say $result->plot_simple;
 
 =head1 DESCRIPTION
 
-TODO
+WebService::IMDBAPI is an interface to L<http://imdbapi.org/>.
+
+=method new
+
+Creates a new WebService::IMDBAPI object. Takes the following optional parameters:
+
+=over 4
+
+=item user_agent
+
+The user agent to use. Note that the default LWP user agent seems to be blocked. Defaults to C<Mozilla/5.0>.
+
+=item language
+
+The language for the results. Defaults to C<en-US>.
+
+=back
+
+=method search_by_title( $title, $options )
+
+Searches based on a title. For the options and their defaults, see L<http://imdbapi.org/>.
+
+Some of the most common options are:
+
+=over 4
+
+=item limit
+
+Limits the number of results.
+
+=back
+
+Returns an array of L<WebService::IMDBAPI::Result> objects.
+
+=method search_by_id( $id, $options )
+
+Searches based on an IMDB ID. For the options and their defaults, see L<http://imdbapi.org/>.
+
+Returns a single L<WebService::IMDBAPI::Result> object.
 
 =cut
--- lib/WebService/IMDBAPI/Result.pm
+++ lib/WebService/IMDBAPI/Result.pm
@@ -17,7 +17,7 @@ use Object::Tiny qw(
   genres
   imdb_id
   imdb_url
-  langauge
+  language
   plot
   plot_simple
   poster
@@ -38,6 +38,52 @@ __END__
 
 =head1 DESCRIPTION
 
-See http://imdbapi.org/#fields for details of the attributes contained in this object. Each field from the API is an attribute.
+See http://imdbapi.org/#fields for details of the attributes contained in this object.
+
+Note that the presence and contents of some of these attributes can differ depending on the options passed to the search.
+
+=attr actors
+
+=attr also_known_as
+
+=attr country
+
+=attr directors
+
+=attr episodes
+
+=attr film_locations
+
+=attr genres
+
+=attr imdb_id
+
+=attr imdb_url
+
+=attr language
+
+=attr plot
+
+=attr plot_simple
+
+=attr poster
+
+=attr rated
+
+=attr rating
+
+=attr rating_count
+
+=attr release_date
+
+=attr runtime
+
+=attr title
+
+=attr type
+
+=attr writers
+
+=attr year
 
 =cut
